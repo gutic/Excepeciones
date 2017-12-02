569b961ae8d32fda266c2bfd66b6e78e918f021a
Agustin Terlaak <agustinterlaak@gmail.com>
2017-12-02 19:21:19 -0300

    asd

--- excepciones/excepciones/Module1.vb
+++ excepciones/excepciones/Module1.vb
@@ -7,11 +7,10 @@ Module Module1
         'Console.WriteLine("algo " & algo)
         'algo = clase1.sumar(3, 2)
         'Console.WriteLine("algo " & algo)
-        Dim ValorRandom As New Random
 
+        Dim ValorRandom As New Random
         For index = 1 To 100 Step 10
             Console.WriteLine("El mayor es: " & mayor(ValorRandom.Next(0, 100), index))
-
         Next
 
     End Sub
